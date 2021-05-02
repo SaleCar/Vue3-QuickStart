6c4d580055df52d572babd3de44aed53a77dcfb0
Saša Gošović <39879027+SaleCar@users.noreply.github.com>
2021-05-02 12:44:51 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import page1 from './page1.js'
 import page2 from './page2.js'
 import page3 from './page3.js'
+import page4 from './page4.js'
 
 export {
     page1,
     page2,
-    page3
+    page3,
+    page4
 }
---
+++ pages/page4.js
@@ -0,0 +1,36 @@
+import component4 from '../components/component4.js'
+
+export default {
+    name: 'Page4',
+    components: {component4},
+
+    setup() {        
+        const {ref} = Vue;
+        const title = 'Page Four'
+        const msgToChild = ref(null)
+        const msgFromChild = ref(null)
+
+        //set message to child component (and reset after 3 secounds)
+        function newMsg() {
+            msgToChild.value = 'Hello Child'
+            setTimeout(function() {msgToChild.value = null}, 3000)
+        }
+
+        //show message from child component 
+        function showMsg(msg) {
+            msgFromChild.value = msg
+        }
+
+        return {title, msgToChild, msgFromChild, newMsg, showMsg}
+    },
+
+    template: `
+        <div>
+            <p>{{ title }}</p>
+            <button v-on:click="newMsg()">Send message to child</button><br>           
+            <p>Message from child: {{msgFromChild}}</p>
+            <br>
+            <component4 :msgProp="msgToChild" @msgEmit='showMsg'></component4>
+        </div>
+    `,
+};
