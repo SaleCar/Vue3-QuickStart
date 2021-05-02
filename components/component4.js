cb6ca79451fd23c43be6424dda0cc3ffe7fe7cf8
Saša Gošović <39879027+SaleCar@users.noreply.github.com>
2021-05-02 12:45:26 +0200

    Add files via upload

---
+++ components/component4.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'Four',
+    
+    props: ['msgProp'],
+    emits: ['msgEmit'],
+    
+
+    setup(props, {emit}) {
+
+        //emit message to parent (and reset after 3 secounds)
+        function newMsg() {
+            emit('msgEmit', 'Hello Parent')
+            setTimeout(function() {emit('msgEmit', '')}, 3000)
+        }
+
+        return {props, newMsg};
+    },
+   
+
+    template: `
+        <h1>Component Four</h1>
+        <button v-on:click="newMsg()">Send message to parent</button><br>
+        <p>Message from page: {{props.msgProp}}</p>
+    `,
+};
